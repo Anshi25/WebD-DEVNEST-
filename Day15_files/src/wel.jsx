9e0272315460f0b9c8495fb1beefbbdb8bb3adf6
Anshika <anshika.1923it1120@kiet.edu>
2021-07-09 17:27:44 +0530

    added

---
+++ Day15_files/src/wel.jsx
@@ -0,0 +1,15 @@
+function navbar()
+{
+  return <h1>WELCOME TO JSX:</h1>;
+}
+ 
+function App(){
+  return (
+    <div>
+    <navbar />
+    <h1>Hello World!!</h1>
+    </div>
+  );
+}
+
+export default App;
