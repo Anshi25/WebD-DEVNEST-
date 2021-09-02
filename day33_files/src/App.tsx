f1204c765e71e126bdacddbbc1192bcbbda6f119
Anshi25 <85192826+Anshi25@users.noreply.github.com>
2021-09-02 15:14:12 +0530

    added

---
+++ day33_files/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useState} from 'react';
+import ThemeContext from './ThemeContext';
+import ThemeToggle from './ThemeToggle';
+import Todos from './Todos';
+import './App.css';
+
+
+const App=()=> {
+  
+  const [theme,setTheme]=useState<boolean>(false);
+  
+  return (
+    <ThemeContext.Provider value={{theme,setTheme}}>
+    
+    <div className="App">
+      <Todos />
+      <ThemeToggle />
+    </div>
+    </ThemeContext.Provider>
+  );
+}
+
+export default App;
